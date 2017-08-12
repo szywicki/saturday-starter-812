c5c25701e6885eb484b38d7fe09a35a826d2510f
Curtis Schlak <curtis.schlak@theironyard.com>
2017-08-12 06:51:09 -0700

    Initial import of an empty Spring Web project.

---
+++ src/main/java/com/theironyard/com/SaturdayStarter812Application.java
@@ -0,0 +1,12 @@
+package com.theironyard.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaturdayStarter812Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaturdayStarter812Application.class, args);
+	}
+}
